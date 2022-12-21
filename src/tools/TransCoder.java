abd5ce83e16b62dd83419f10b1a5f389ca9fe7ff
Dairalir <daimanvm@gmail.com>
2022-12-21 09:52:34 +0100

    creation encode

--- src/tools/TransCoder.java
+++ src/tools/TransCoder.java
@@ -1,9 +1,6 @@
 package tools;
 
 import org.germain.tool.ManaBox;
-import org.junit.Assert;
-import org.junit.Test;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,37 +8,41 @@ public class TransCoder {
     private Map<Character,String> encode = new HashMap<Character,String>();
     private Map<String,Character> decode = new HashMap<String,Character>();
 
+
+
     public TransCoder(String keyCrypted) {
         String key = ManaBox.decrypt(keyCrypted);
 
+        for (int i = 0; i < key.length(); i++){
+            encode.put(key.charAt(i),getLetterCode(i+26));
+            decode.put(getLetterCode(i+26),key.charAt(i));
         }
+    }
+    public static String getLetterCode(int i) {
+        return i < 0 ? "" : getLetterCode((i / 26) - 1) + (char)(65 + i % 26);
+    }
 
     public Map<Character, String> getEncode() {
-
         return encode;
     }
     public Map<String, Character> getDecode() {
-
         return decode;
     }
     
     public String encode(String msg){
-
-        return msg;
+        String buffer = "";
+        for(char ch: msg.toCharArray()){
+            buffer += encode.get(ch);
+        }
+        return buffer;
     }
 
     public String decode(String msg){
-
-        return msg;
+        String buffer = "";
+        for (char ch: msg.toCharArray()){
+            buffer += decode.get(ch);
+        }
+        return buffer;
     }
 
-    @Test
-    public void createMapTest() {
-        String testGermain = "6lUjKOzUj4e/Gelw9c6sDLqHniwulClN6XSayZ+HRF/kbZx+CMf95jxrhm4YFSY26OnxVlsrzGkO00IMeAFs3g==";
-        TransCoder transcodeTest = new TransCoder(testGermain);
-        Assert.assertNotNull(transcodeTest.getEncode());
-        System.out.println(transcodeTest.getEncode().toString());
-        Assert.assertNotNull(transcodeTest.getDecode());
-        System.out.println(transcodeTest.getDecode().toString());
-    }
 }
