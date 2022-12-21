5cc207fe39d2476d9942806b8228448d49a5a8ff
Dairalir <daimanvm@gmail.com>
2022-12-21 16:59:42 +0100

    creation booleen encoded

--- src/model/Message.java
+++ src/model/Message.java
@@ -1,17 +1,21 @@
 package model;
 
 
+import tools.TransCoder;
+
 import java.util.Scanner;
 
 public class Message {
     public boolean encoded() {
+        String keyCrypted = "6Qe0IsEEH1utWRe7UKzGMiDTytOB3HS1dEfIB4imna3IRHXHRn5ZrvKFEcPjmPgKYGuytG+gDAl1m2DdHalJQg==";
         Scanner sc = new Scanner(System.in);
+        TransCoder tc = new TransCoder(keyCrypted);
         System.out.println("Entrez votre message");
         String msg = sc.nextLine();
 
         Boolean b1 = true;
         Boolean b2 = false;
-        if(msg.){
+        if(tc.decode(msg) == ){
             b2 = true;
             if(b1.equals(b2)){
                 System.out.println("Le message est encoder");
