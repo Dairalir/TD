b4e268a196ed2bc2528b45ddfc18f784d090ebce
Dairalir <daimanvm@gmail.com>
2022-12-21 16:44:40 +0100

    creation booleen encoded

--- src/model/Message.java
+++ src/model/Message.java
@@ -1,4 +1,24 @@
 package model;
 
+
+import java.util.Scanner;
+
 public class Message {
+    public boolean encoded() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrez votre message");
+        String msg = sc.nextLine();
+
+        Boolean b1 = true;
+        Boolean b2 = false;
+        if(msg.){
+            b2 = true;
+            if(b1.equals(b2)){
+                System.out.println("Le message est encoder");
+            }
+        }else{
+            System.out.println("Le message n'est pas encoder");
+        }
+    }
 }
+
