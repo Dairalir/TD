9a0ac33e9f88997a140641dbbc5ec4bca40dde3d
Dairalir <daimanvm@gmail.com>
2022-12-22 16:49:09 +0100

    Creation methode readNwrite

--- src/model/Message.java
+++ src/model/Message.java
@@ -6,12 +6,11 @@ import tools.TransCoder;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Message {
     private Boolean encoded;
     private List<String> msgClear;
-    private List<String> msgEncode;
+    private List<String> msgEncoded;
     private Path msgClearPath;
     private Path msgEncodedPath;
     private Path keyPath;
@@ -24,11 +23,27 @@ public class Message {
         this.msgEncodedPath = msgEncodedPath;
         this.keyPath = keyPath;
         msgClear = new ArrayList<>();
-        msgEncode = new ArrayList<>();
+        msgEncoded = new ArrayList<>();
         this.key = this.generateKey();
         this.transCoder = new TransCoder(key);
 
     }
+    private void readNwrite() {
+        if (encoded) {
+            for (String ligne : msgEncoded) {
+                String ligneDecoded = transCoder.decode(ligne);
+                System.out.println(ligneDecoded);
+            }
+            System.out.println("Le message décodé se trouve: " + msgClearPath.toString());
+        } else {
+            for (String ligne : msgClear) {
+                String ligneEncoded = transCoder.encode(ligne);
+            }
+            System.out.println("Le message encodé se trouve: " + msgEncodedPath.toString());
+        }
+    }
+
+
     private String generateKey() {
         return "6Qe0IsEEH1utWRe7UKzGMiDTytOB3HS1dEfIB4imna3IRHXHRn5ZrvKFEcPjmPgKYGuytG+gDAl1m2DdHalJQg==";
     }
